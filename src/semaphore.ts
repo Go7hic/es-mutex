3aa4d866db2fed74b330930a277b490cdb9224d9
Go7hic <gtfx0209@qq.com>
2020-11-24 16:00:06 +0800

    update

--- src/semaphore.ts
+++ src/semaphore.ts
@@ -1,43 +1,47 @@
-export function Semaphore(max: number): any {
-  let counter = 0;
-  let waiting = [];
+class Semaphore {
+  max: number;
+  constructor(max:number) {
+    this.max = max
+  }
 
-  const take = () => {
-    if (waiting.length > 0 && counter < max) {
-      counter++;
-      const promise = waiting.shift();
+  private waiting: any[] = [];
+  private counter: number = 0;
+  private take() {
+    if (this.waiting.length > 0 && this.counter < this.max) {
+      this.counter++;
+      const promise = this.waiting.shift();
       promise.resolve();
     }
   }
-
-  this.acquire = ():Promise<[]> => {
-    if (counter < max) {
-      counter++
+  public acquire() {
+    if (this.counter < this.max) {
+      this.counter++
       return new Promise(resolve => {
         resolve();
       });
     } else {
       return new Promise((resolve, err) => {
-        waiting.push({ resolve, err });
+        this.waiting.push({ resolve, err });
       });
     }
   }
-
-  this.release = () => {
-    counter--;
-    take();
+  public release() {
+    this.counter--;
+    this.take();
   }
 
-  this.purge = (): number => {
-    const unresolved = waiting.length;
+  public purge() {
+      const unresolved = this.waiting.length;
+      for (let i = 0; i < unresolved; i++) {
+        this.waiting[i].err('Task has been purged.');
+      }
+      this.counter = 0;
+      this.waiting = [];
 
-    for (let i = 0; i < unresolved; i++) {
-      waiting[i].err('Task has been purged.');
+      return unresolved;
     }
 
-    counter = 0;
-    waiting = [];
 
-    return unresolved;
-  }
 }
+
+export { Semaphore }
